96990f5e1eceffba32c83b7f9f3ae1f5b31e7121
Pătru Gabriela-Andreea <patru.gabriela.h6r@student.ucv.ro>
2022-12-18 23:58:44 +0200

    Add project files.

---
+++ ShoesStore/Models/Produs.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoesStore.Models
+{
+    public class Produs
+    {
+        [Key]
+        public int Id_Produs { get; set; }
+        public string Nume_Model { get; set; }
+        public string Tip { get; set; }
+        public string? ImageURL { get; set; }
+
+        public Brand? Brand { get; set; }
+        public int Id_Brand { get; set; }
+
+    }
+}
---
+++ ShoesStore/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoesStore.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id_User { get; set; }
+        public string Origine_User { get; set; }
+       
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Parola { get; set; }
+        
+
+    }
+}
